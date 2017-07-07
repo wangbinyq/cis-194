294d8900506735acfc8ec476cd4b3caaff6d0002
wangbin <wangb@nextechat.com>
2017-07-07 19:26:28 +0800

    update insert

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -12,4 +12,12 @@ parseMessage str = case words str of
 parse :: String -> [LogMessage]
 parse str = [parseMessage(line) | line <- lines str]
 
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mt = mt
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ nt _) (Node lt mt@(LogMessage _ t _) rt) = 
+  case nt < t of
+    True -> Node (insert msg lt) mt rt
+    False -> Node lt mt (insert msg rt)
+
 main = testParse parse 10 "error.log"
